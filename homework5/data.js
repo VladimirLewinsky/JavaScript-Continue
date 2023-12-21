f8054a3fbd104e30dd2967a3ac5b58593b9ad6ea
Vladimir <112421307+VladimirLewinsky@users.noreply.github.com>
2023-12-21 20:07:45 +0300

    Add files via upload

---
+++ homework5/data.js
@@ -0,0 +1,62 @@
+const dataBase = `[
+    {
+      "type": "general",
+      "setup": "What was the pumpkin’s favorite sport?",
+      "punchline": "Squash.",
+      "id": 268
+    },
+    {
+      "type": "general",
+      "setup": "What time is it?",
+      "punchline": "I don't know... it keeps changing.",
+      "id": 78
+    },
+    {
+      "type": "programming",
+      "setup": "Why do C# and Java developers keep breaking their keyboards?",
+      "punchline": "Because they use a strongly typed language.",
+      "id": 74
+    },
+    {
+      "type": "general",
+      "setup": "What does the mermaid wear to math class?",
+      "punchline": "Algae-bra.",
+      "id": 401
+    },
+    {
+      "type": "general",
+      "setup": "Did you hear about the cow who jumped over the barbed wire fence?",
+      "punchline": "It was udder destruction.",
+      "id": 88
+    },
+    {
+      "type": "general",
+      "setup": "How did Darth Vader know what Luke was getting for Christmas?",
+      "punchline": "He felt his presents.",
+      "id": 117
+    },
+    {
+      "type": "general",
+      "setup": "What did the judge say to the dentist?",
+      "punchline": "Do you swear to pull the tooth, the whole tooth and nothing but the tooth?",
+      "id": 176
+    },
+    {
+      "type": "general",
+      "setup": "How many bones are in the human hand?",
+      "punchline": "A handful of them.",
+      "id": 139
+    },
+    {
+      "type": "general",
+      "setup": "What's the worst thing about ancient history class?",
+      "punchline": "The teachers tend to Babylon.",
+      "id": 279
+    },
+    {
+      "type": "general",
+      "setup": "Is there a hole in your shoe?",
+      "punchline": "No… Then how’d you get your foot in it?",
+      "id": 149
+    }
+  ]`;
